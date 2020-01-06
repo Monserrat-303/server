fa02fea6c273aa26f05bc9d777eae41515d81e8e
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-01-06 16:07:19 -0300

    second

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const port = process.env.PORT || 4000;
+const app = require('./app');
+
+const server = http.createServer(app);
+console.log('running on port', port)
+server.listen(port);
