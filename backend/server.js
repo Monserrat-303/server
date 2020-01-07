1d46daf066ec09fadada9e77392da30950802506
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-01-07 11:11:46 -0300

    adding graphQL

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const port = process.env.PORT || 4000;
 const app = require('./app');
+const holamundo = require('./api/holamundo');
 
-const server = http.createServer(app);
+const server = http.createServer(holamundo);
 console.log('running on port', port)
 server.listen(port);
